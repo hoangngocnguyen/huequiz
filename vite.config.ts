2ad4414c9ab93fa36f673238e99b368047c716fc
hoangngocnguyen <142553243+hoangngocnguyen@users.noreply.github.com>
2026-06-23 15:34:06 +0700

    fix (route): edit base project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <-- ĐỔI DÒNG NÀY (từ @vitejs/react-vite thành @vitejs/plugin-react)
+import react from '@vitejs/plugin-react' 
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: "/huequiz",
+  base: "/huequiz/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
